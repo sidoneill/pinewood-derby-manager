6f9ce6297aa288c5dda8fa1836c420101ba4b13b
Sid O'Neill <sid@madebyhighland.com>
2025-03-07 15:52:05 -0500

    Update main.js to load data from localStorage

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 import './style.css';
-import { addRacer, getRacers, clearRacers } from './js/racers.js';
-import { generateRaces, getCurrentRace, getCompletedRaces, markRaceComplete } from './js/races.js';
+import { loadRacers } from './js/racers.js';
+import { loadRaces } from './js/races.js';
 import { calculateResults } from './js/results.js';
 import { setupUI, updateUI } from './js/ui.js';
 
 // Initialize the application
 document.addEventListener('DOMContentLoaded', () => {
+  // Load data from localStorage
+  loadRacers();
+  loadRaces();
+  calculateResults();
+  
+  // Set up UI event handlers
   setupUI();
+  
+  // Update the UI with loaded data
   updateUI();
 });
